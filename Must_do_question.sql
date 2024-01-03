88c53112f23f768a8784217b81029fb089a9648e
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-03 12:00:08 +0530

    Update Must_do_question.sql

--- Must_do_question.sql
+++ Must_do_question.sql
@@ -6,8 +6,20 @@ SELECT * FROM employee ORDER BY salary DESC LIMIT 2,1;
 # NTH HIGHEST SALARY
 SELECT salary FROM employee e1 where N-1 = (SELECT COUNT(DISTINCT SALARY) FROM employee e2 where e1.salary <e2.salary)
   
-# FIND DUPLICATES
-  
+# FIND DUPLICATES ROW
+  SELECT * ,COUNT(empid) from Employee GROUP BY empid;
+
+# RETRIVE EVEN  ROWS FROM TABLE
+  SELECT * from Employee WHERE MOD(empid ,2)=0;
+
+# RETRIVE ODD ROWS FROM TABLE
+  SELECT * from Employee WHERE MOD(empid ,2)=1;
+
+
+# SELECT FIRST AND LAST ROW 
+  SELECT * FROM Employee WHERE empid = (SELECT MIN(empid) from Employee);
+
+  SELECT * FROM Employee WHERE empid = (SELECT MAX(empid) from Employee);
 
 # REMOVE DUPLICATES
 
