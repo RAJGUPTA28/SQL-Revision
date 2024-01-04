dabcc9af15a8fd7dca314f0d4fd9a9d6313c1700
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-04 12:00:09 +0530

    Update Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -2,3 +2,13 @@
 SELECT StudentCourse.COURSE_ID, Student.NAME, Student.AGE FROM Student
 INNER JOIN StudentCourse
 ON Student.ROLL_NO = StudentCourse.ROLL_NO;
+
+
+
+# LEFT JOIN
+SELECT Student.NAME,StudentCourse.COURSE_ID 
+FROM Student
+LEFT JOIN StudentCourse 
+ON StudentCourse.ROLL_NO = Student.ROLL_NO;
+
+# RIGHT JOIN
